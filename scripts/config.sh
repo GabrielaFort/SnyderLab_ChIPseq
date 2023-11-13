08b94338ff86c275b179e588d0c559a54c295575
Gabriela Fort <u1256293@kingspeak1.wasatch.peaks>
2023-11-13 11:12:48 -0700

    adding config script

---
+++ scripts/config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get and install miniconda3 into home directory
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/software/pkg/miniconda3 -s
+
+# Create a user environment module
+mkdir -p $HOME/MyModules/miniconda3
+cp /uufs/chpc.utah.edu/sys/installdir/python/modules/miniconda3/latest.lua $HOME/MyModules/miniconda3
+
+# Load chipseq conda environment for chipseq analysis
+module use $HOME/MyModules
+module load miniconda3/latest
+
+conda env create --file ../files/chipseq.yaml --name chipseq
