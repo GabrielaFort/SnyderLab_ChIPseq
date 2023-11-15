a1d66a57fa73410c025974ab14eeebb6d673d990
Gabriela Fort <u1256293@kingspeak2.wasatch.peaks>
2023-11-15 09:59:55 -0700

    changed replciates script to accept directories

--- scripts/combine_replicates.sh
+++ scripts/combine_replicates.sh
@@ -5,8 +5,8 @@
 
 # loading modules
 module load bedtools
-
-# must have homer installed and in path
+module use $HOME/MyModules/miniconda3
+# must have homer installed and in path as well! 
 
 ###### Parsing command line arguments ######
 arg0=$(basename "$0")
@@ -14,8 +14,8 @@ arg0=$(basename "$0")
 # Usage function to describe/document script
 usage_info()
 {
-    echo "Usage: $arg0 [{-a|--abed} Bed_R1] [{-b|--bbed} Bed_R2] [{-as|--asummit} Summit_R1] [{-bs|--bsummit} Summit_R2] [{-o|--output} name] [{-g|--genome} genome]"
-    echo -e "\nThis bash script will take bed and summit bed files from\ntwo replicates, a reference genome, and an output directory name.\nIt will return bed files, annotations and HOMER analysis on\nonly overlapping peaks between the two replicates."
+    echo "Usage: $arg0 [{-a|--rep1} Replicate1] [{-b|--rep2} Replicate2] [{-o|--output} name] [{-g|--genome} genome]"
+    echo -e "\nThis bash script will take paths to Macs2 output directories from\ntwo replicates, a reference genome, and an output directory name.\nIt will return bed files, merged bw files, and annotations and HOMER analysis on\nonly overlapping peaks between the two replicates. It will also use deeptools to\ncreate tornado plots of the two replicates side by side."
 }
 
 
@@ -33,10 +33,8 @@ help_fun()
 {
     usage_info
     echo
-    echo "{-a|--abed} R1_bed              -- R1 bed file"
-    echo "{-b|--bbed} R2_bed              -- R2 bed file"
-    echo "{-as|--asummit} R1_summit       -- R1 summit bed file"
-    echo "{-bs|--bsummit} R1_summit       -- R2 summit bed file"
+    echo "{-a|--rep1} Replicate1          -- Path to peak calling output directory for Rep 1"
+    echo "{-b|--rep2} Replicate2          -- Path to peak calling output directory for Rep2"
     echo "{-g|--genome} genome            -- Input genome that files are aligned to (mm10, mm39, hg19, hg38)"
     echo "{-o|--output} qvalue            -- Set name of output directory" 
     echo "{-h|--help}                     -- Prints this help message and exits"
@@ -53,25 +51,15 @@ flags()
     while test $# -gt 0
     do
         case "$1" in
-        (-a|--abed)
+        (-a|--rep1)
             shift
             [ $# = 0 ] && error "No R1 bed file specified"
-            export bed_r1="$1"
+            export r1_path="$1"
             shift;;
-        (-b|--bbed)
+        (-b|--rep2)
             shift
             [ $# = 0 ] && error "No R2 bed file specified"
-            export bed_r2="$1"
-            shift;;
-        (-as|--asummit)
-            shift
-            [ $# = 0 ] && error "No R1 summit file specified"
-            export summit_r1="$1"
-            shift;;
-        (-bs|--bsummit)
-            shift
-            [ $# = 0 ] && error "No R2 summit file specified"
-            export summit_r2="$1"
+            export r2_path="$1"
             shift;;
         (-g|--genome)
             shift
@@ -101,41 +89,25 @@ then
 fi
 
 
-# Make sure input files exist
-if [ ! -f $bed_r1 ]
+# Make sure input directories exist
+if [ ! -d $r1_path ]
 then
-  echo "Cannot find R1 bed file"
+  echo "Cannot find R1 directory OR input is not a directory"
   exit 1
-elif [ ! -f $bed_r2 ]
+elif [ ! -d $r2_path ]
 then
-  echo "Cannot find R2 bed file"
-  exit 1
-elif [ ! -f $summit_r1 ]
-then
-  echo "Cannot find R1 summit file"
-  exit 1
-elif [ ! -f $summit_r2 ]
-then 
-  echo "Cannot find R2 summit file"
+  echo "Cannot find R2 directory OR input is not a directory"
   exit 1
 fi
 
 # Make sure all input arguments have been assigned to variables
-if [ ! -v bed_r1 ]
+if [ ! -v r1_path ]
 then
-  echo '-a|--abed is a required argument'
+  echo '-a|--rep1 is a required argument'
   exit 1
-elif [ ! -v bed_r2 ]
+elif [ ! -v r2_path ]
 then
-  echo '-b|--bbed is a required argument'
-  exit 1
-elif [ ! -v summit_r1 ]
-then
-  echo '-as|--asummit is a required argument'
-  exit 1
-elif [ ! -v summit_r2 ]
-then
-  echo '-bs|--bsummit is a required argument'
+  echo '-b|--rep2 is a required argument'
   exit 1
 elif [ ! -v genome ]
 then
@@ -146,6 +118,17 @@ then
   echo '-o|--output is a required argument'
   exit 1
 fi
+
+
+# Find and assign files to variables
+base_r1=$(basename $r1_path)
+base_r2=$(basename $r2_path)
+bed_r1=$(${r1_path}/${base_r1}_peaks.narrowPeak)
+bed_r2=$(${r2_path}/${base_r2}_peaks.narrowPeak)
+bw_r1=$(${r1_path}/${base_r1}.bw)
+bw_r2=$(${r2_path}/${base_r2}.bw)
+summit_r1=$(${r1_path}/${base_r1}_summits.bed)
+summit_r2=$(${r2_path}/${base_r2}_summits.bed)
 
 
 # Make directory with input name and navigate into it
@@ -166,6 +149,13 @@ else
   bedtools intersect -a ../${summit_r2} -b ${output}_intersect.bed -wa > ${output}_intersect_summits.bed
 fi
 
+# create merged bw files
+echo -e "Creating merged bw files...\n" >> combinereps_summary.out
+bigWigMerge ../${bw_r1} ../${bw_r2} merged.bedGraph
+bedGraphToBigWig merged.bedGraph $genome merged.bw
+
+
+
 echo -e "Annotating peaks...\n" >> combinereps_summary.out
 # Annotate peaks
 annotatePeaks.pl ${output}_intersect.bed $genome -go ./GO_intersect -annStats ${output}_intersect_annotation.log > ${output}_intersect_annotation.txt
@@ -179,16 +169,14 @@ sort -k1,1 -k2,2n ${output}_intersect_summits.bed | uniq | awk '{print $1,$2-50,
 
 
 
-
-
 # Now run homer to find enriched motifs
 findMotifsGenome.pl ./homer/${output}.sorted.bed $genome ./homer -size 100 -mask -preparse -p 16
 
 echo -e "Adding peak annotations to bed file...\n" >> combinereps_summary.out
 # Add annotated peaks to merged bed files with python script
-module use $HOME/MyModules/miniconda3
-module load miniconda3/latest
+
 source $HOME/software/pkg/miniconda3/etc/profile.d/conda.sh 
+# Activate conda env
 conda activate chipseq
 
 # Launch python script with appropriate command line options (will be parsed from within the script)
@@ -200,7 +188,7 @@ conda deactivate
 num_peaks=$(wc -l ${output}_intersect.bed) 
 echo -e "---------------------------------------\nJob Finished at: `date`\nNumber of intersected peaks: $num_peaks\n---------------------------------------" >> combinereps_summary.out
 
-
+# Create heatmap of both replicates side by side
 
 
 
