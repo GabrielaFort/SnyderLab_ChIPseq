f345f233780b9574f112b91c99e7e880a899ca3f
Gabriela Fort <u1256293@kingspeak2.wasatch.peaks>
2023-11-15 10:00:59 -0700

    updating deep tools script

--- scripts/deeptoolsplots.sh
+++ scripts/deeptoolsplots.sh
@@ -2,15 +2,6 @@
 
 
 
-computeMatrix reference-point \ # choose the mode
-       --referencePoint center \ # alternatives: TES, center
-       -b 1500 -a 1500 \ # define the region you are interested in
-       -R $1 \ #bed file
-       -S $2  \ #bw file
-       --skipZeros \
-       -o matrix1.gz \ # to be used with plotHeatmap and plotProfile
+computeMatrix reference-point --referencePoint center -b 1500 -a 1500 -R $1 -S $2 --skipZeros -o matrix1.gz  # to be used with plotHeatmap and plotProfile
 
-plotHeatmap \
-       -m matrix1.gz\
-       -out testm1.pdf \
-       --heatmapHeight 15  
+plotHeatmap -m matrix1.gz -out testm1.pdf --heatmapHeight 15  
